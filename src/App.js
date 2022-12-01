b34b047be710003a05b3f85b4d9110ad6b7e7930
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-01 13:53:36 +0530

    01st Dec 2022-Login-&-SignUp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,25 +11,61 @@ import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "../src/user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/:userId/places" element={<UserPlaces />} />
+        <Route exact path="/places/new" element={<NewPlace />} />
+        <Route exact path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  } else {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/:userId/places" element={<UserPlaces />} />
+        <Route exact path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+      </Routes>
+    );
+  }
+
   return (
-    <Router>
-      {/* as our MainNavigation component is using Link component from react-dom-router, we can include it inside Router*/}
-      <MainNavigation />
-      <main>
-        {/* main used for styling, as defined to the stylesheet */}
-        <Routes>
-          <Route exact path="/" element={<Users />} />
-          {/* route with dynamic segment, /: signals the router */}
-          <Route exact path="/:userId/places" element={<UserPlaces />} />
-          <Route exact path="/places/new" element={<NewPlace />} />
-          <Route exact path="/places/:placeId" element={<UpdatePlace />} />
-          <Route exact path="/auth" element={<Auth />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          {/* <Routes> */}
+            {/* <Route exact path="/" element={<Users />} />
+            <Route exact path="/:userId/places" element={<UserPlaces />} />
+            <Route exact path="/places/new" element={<NewPlace />} />
+            <Route exact path="/places/:placeId" element={<UpdatePlace />} />
+            <Route exact path="/auth" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/" replace />} /> */}
+            {routes}
+          {/* </Routes> */}
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
