6066098f3e2ddcb86ef19824a38812b1a257090a
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-01 10:04:07 +0530

    01st Dec 2022

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -4,13 +4,15 @@ import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
 import "./PlaceList.css";
 
+import Button from "../../shared/components/FormComponents/Button";
+
 const PlaceList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="place-list center">
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <button>Share Place</button>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
